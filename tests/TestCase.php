e07d598e53f3ef4437c92b429cbeb6f67507ab4f
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-28 09:55:37 +0100

    ✅ Use testbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,83 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Loom\Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Illuminate\Contracts\Config\Repository;
+use Loom\Loom;
+use Loom\LoomServiceProvider;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use Spatie\LaravelRay\RayServiceProvider;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        // Code before application created.
+
+        parent::setUp();
+
+        $this->artisan('optimize:clear');
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int,class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LoomServiceProvider::class,
+            RayServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Override application aliases.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<string, class-string<\Illuminate\Support\Facades\Facade>>
+     */
+    protected function getPackageAliases($app): array
+    {
+        return [
+            'Loom' => Loom::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
+    {
+        tap($app->make('config'), function (Repository $config) {
+            $config->set('app.debug', false);
+            $config->set('database.default', 'testbench');
+            $config->set('database.connections.testbench', [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+            ]);
+        });
+    }
+
+    /**
+     * Define routes setup.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function defineRoutes($router)
+    {
+        // Define routes.
+    }
+}
