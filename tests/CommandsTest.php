3f3c9402658dc3b58e08fd3f184d6813be301628
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-28 09:41:35 +0100

    🧪 Add command tests

---
+++ tests/CommandsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Filesystem\Filesystem;
+use Loom\Commands\Aliases\InstallCommand as InstallCommandAlias;
+use Loom\Commands\Aliases\MakeColumnCommand as MakeColumnCommandAlias;
+use Loom\Commands\Aliases\MakeFieldCommand as MakeFieldCommandAlias;
+use Loom\Commands\InstallCommand;
+use Loom\Commands\MakeColumnCommand;
+use Loom\Commands\MakeFieldCommand;
+
+test('aliases should expand commands', function () {
+    $files = new Filesystem;
+
+    expect(new InstallCommandAlias)->toBeInstanceOf(InstallCommand::class);
+    expect(new MakeColumnCommandAlias($files))->toBeInstanceOf(MakeColumnCommand::class);
+    expect(new MakeFieldCommandAlias($files))->toBeInstanceOf(MakeFieldCommand::class);
+});
