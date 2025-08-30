c44e3f850bceb18889fa8cc1c8159431c7b37323
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-30 03:20:14 +0100

    ✅ Test the `loom:install` command

--- tests/CommandsTest.php
+++ tests/CommandsTest.php
@@ -3,17 +3,21 @@
 declare(strict_types=1);
 
 use Illuminate\Filesystem\Filesystem;
-use Loom\Commands\Aliases\InstallCommand as InstallCommandAlias;
 use Loom\Commands\Aliases\MakeColumnCommand as MakeColumnCommandAlias;
 use Loom\Commands\Aliases\MakeFieldCommand as MakeFieldCommandAlias;
-use Loom\Commands\InstallCommand;
 use Loom\Commands\MakeColumnCommand;
 use Loom\Commands\MakeFieldCommand;
+
+test('install command', function () {
+    $this->artisan('loom:install')
+        ->expectsQuestion('Would you like to star our repo on GitHub?', true)
+        ->expectsOutput('Loom installed successfully ✅')
+        ->assertSuccessful();
+});
 
 test('aliases should expand commands', function () {
     $files = new Filesystem;
 
-    expect(new InstallCommandAlias)->toBeInstanceOf(InstallCommand::class);
     expect(new MakeColumnCommandAlias($files))->toBeInstanceOf(MakeColumnCommand::class);
     expect(new MakeFieldCommandAlias($files))->toBeInstanceOf(MakeFieldCommand::class);
 });
