2d5ea1de3e09e09d36ba6bdfc7bfe812be10d313
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-09-04 20:28:10 +0100

    ✨ Add name and slug group

---
+++ packages/core/src/Components/Fields/NameAndSlugGroup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Loom\Components\Fields;
+
+use Filament\Schemas\Components\Group;
+use Illuminate\Support\Str;
+
+class NameAndSlugGroup
+{
+    public static function make(?string $name = null, ?string $slug = null): Group
+    {
+        $name ??= 'name';
+        $slug ??= 'slug';
+
+        return Group::make([
+            NameField::make($name)
+                ->reactive()
+                ->live(onBlur: true)
+                ->afterStateUpdated(fn ($state, callable $set) => $set($slug, Str::slug($state))),
+            SlugField::make($slug),
+        ])
+            ->columnSpanFull()
+            ->columns(2);
+    }
+}
