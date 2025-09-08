9c6a702d43bba434e3109563ee0a02250353e30d
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-09-08 04:41:17 +0100

    ♻️ Improve relationship fields

--- packages/core/src/Components/Fields/OrganizationField.php
+++ packages/core/src/Components/Fields/OrganizationField.php
@@ -7,17 +7,17 @@ namespace Loom\Components\Fields;
 use Closure;
 use Filament\Forms\Components\Select;
 
-class OrganizationField extends BelongsToNamedField
+class OrganizationField extends RelatedToField
 {
     public static function make(
-        ?string $name = 'organization',
-        string|Closure|null $titleAttribute = 'name',
+        ?string $name = null,
+        string|Closure|null $titleAttribute = null,
         ?Closure $modifyQueryUsing = null,
         bool $ignoreRecord = false
     ): Select {
         return parent::make(
-            $name ?? config('loom.components.organization.name', 'organization'),
-            $titleAttribute,
+            $name ?? config('loom.components.organization.name', 'organization_id'),
+            $titleAttribute ?? config('loom.components.organization.title_attribute', 'name'),
             $modifyQueryUsing,
             $ignoreRecord
         )
