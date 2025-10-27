2122344df341e9b756a479c709e5d7ffc4896a36
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-10-27 21:23:40 +0100

    ✨ Show icon preview

--- packages/core/src/Components/Fields/IconField.php
+++ packages/core/src/Components/Fields/IconField.php
@@ -7,6 +7,7 @@ namespace Loom\Components\Fields;
 use Filament\Forms\Components\Select;
 use Filament\Support\Icons\Heroicon;
 use Loom\Rules\IconRule;
+use Sikessem\UI\Facade as UI;
 
 class IconField extends Field
 {
@@ -15,9 +16,26 @@ class IconField extends Field
         $name ??= loom()->config('components.icon.name', 'icon');
 
         return Select::make($name)
-            ->options(array_column(Heroicon::cases(), 'value', 'value'))
+            ->options(function () {
+                $icons = array_column(Heroicon::cases(), 'value', 'value');
+
+                foreach ($icons as $name => $icon) {
+                    $icon = str_starts_with($icon, 'o-')
+                        ? "heroicon-{$icon}"
+                        : "heroicon-s-{$icon}";
+                    $icons[$name] = UI::render(<<<HTML
+                        <span class="inline-flex items-center justify-center gap-1">
+                            <x-filament::icon icon='{$icon}'/>
+                            {$name}
+                        </span>
+                    HTML);
+                }
+
+                return $icons;
+            })
             ->rule(new IconRule)
             ->searchable()
+            ->allowHtml()
             ->label(loom()->trans('components.icon'));
     }
 }
